eec58ae1041dc1597a61e953ec387bf7bd0f23d2
Artem Klimov <artyomklimov@yandex.ru>
2025-01-26 18:19:48 +0300

    Initial commit

---
+++ src/main/java/com/javarush/domain/City.java
@@ -0,0 +1,27 @@
+package com.javarush.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(schema = "world", name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private String district;
+
+    private Integer population;
+
+}
+
